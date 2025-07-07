799119d47a6ed773043c1ed447379b67455031a3
manu <manuellatourf@gmail.com>
2025-07-07 14:19:38 +0200

    test4

--- server/github.js
+++ server/github.js
@@ -1,4 +1,4 @@
-const fetch = require("node-fetch");
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const owner = "meowrhino"; // ← tu usuario de GitHub
 const repo = "messagePark"; // ← el repo donde está mensajes.json
